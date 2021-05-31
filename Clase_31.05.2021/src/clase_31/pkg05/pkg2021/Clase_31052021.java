e03d1a0c1364b166cc8cc4902e4a3935fb1fca97
Jorge <Jorge@Galleon-A301856>
2021-05-31 14:27:35 -0500

    Primera versión Clase_31052021

---
+++ Clase_31.05.2021/src/clase_31/pkg05/pkg2021/Clase_31052021.java
@@ -0,0 +1,12 @@
+
+package clase_31.pkg05.pkg2021;
+
+
+public class Clase_31052021 {
+
+
+    public static void main(String[] args) {
+        System.out.println("Taller de conexión NetBeans GitHub");
+    }
+    
+}
